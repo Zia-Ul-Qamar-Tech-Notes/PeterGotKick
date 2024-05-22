6b42d185f19c57e494ff3d934fccaf2958d67e29
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2024-05-22 09:35:54 +0500

    asdone

--- server.js
+++ server.js
@@ -7,12 +7,8 @@ const path = require("path");
 // Serve static files (images, CSS, etc.) from the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Define route for fetching product prices
