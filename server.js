46abe189968d7898359346d79953b004a43fda4a
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2024-05-22 08:09:29 +0500

    dggasdhj

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/product/:productId", (req, res) => {
 });
 
 // Handle other routes
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
